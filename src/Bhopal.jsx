950e3ea6ae4b8f47ddcb3d3052ecadbb7a1c4a85
tanmay493 <tanmaylanjewar24874@gmail.com>
2026-02-07 16:34:39 +0530

    router

---
+++ src/Bhopal.jsx
@@ -0,0 +1,11 @@
+import Cybrom from "./Cybrom";
+
+const Bhopal=()=>{
+    return(
+        <>
+        <h1 className="data">we learn from bhopal</h1>
+        <Cybrom/>
+        </>
+    )
+}
+export default Bhopal;
---
+++ src/Cybrom.jsx
@@ -0,0 +1,10 @@
+import Bhopal from "./Bhopal";
+const Cybrom=()=>{
+    return(
+        <>
+        <h1 id="data1">we learn from cybrom</h1>
+        <Bhopal/>
+        </>
+    )
+}
+export default Cybrom;
