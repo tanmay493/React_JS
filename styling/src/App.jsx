9de266f551bb36db4bfc50fb2cbef0b11894a4a9
tanmay493 <tanmaylanjewar24874@gmail.com>
2026-03-17 10:48:17 +0530

    styling

---
+++ styling/src/App.jsx
@@ -0,0 +1,29 @@
+import "./App.css"
+ 
+function App(){
+  let mystyle={
+    color:"blue",
+    backgroundColor:"pink",
+    padding:"10px"
+  }
+  return(
+    
+    <>
+    <h1 style={{backgroundColor:"green",color:"white"}}>hello</h1>
+    <h2 style={{backgroundColor:"yellow",padding:"30px"}}>welcome</h2>
+    <h3 style={{backgroundColor:"red"}}>everyone</h3>
+    <h4 style={mystyle}>cybrom</h4>
+    <h5>Bhopal</h5>
+    <hr />
+    <div className="container">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus hic voluptatem fuga quia nostrum sit, accusamus sapiente, quisquam iure totam enim deserunt dignissimos, perspiciatis ut culpa odio incidunt tempore voluptatum.
+      Voluptas quos impedit neque recusandae quod laboriosam odio ut, officiis quidem deleniti facere laudantium. Dolore repellat ipsum, voluptate nam, eaque deserunt accusamus odio, eligendi ea fugit libero delectus esse officia.
+      Fugiat dolor culpa repellat vel ipsam, aliquid unde ipsum dicta quidem, saepe neque delectus aspernatur facere veritatis ducimus nulla accusamus minus sit quia iusto! Magni voluptatem animi magnam? Rerum, voluptas.
+      Voluptate, laudantium impedit ipsam labore necessitatibus obcaecati laborum, asperiores maiores similique nihil assumenda natus eaque veritatis nobis, in ea! Vero eius voluptatibus quod perspiciatis nihil nisi tenetur assumenda, totam eligendi.
+      Delectus pariatur quis illo perferendis laborum autem porro similique est molestias, impedit aliquam at adipisci veritatis nostrum. Fugit, mollitia deleniti culpa porro facilis vel tenetur magni laudantium blanditiis animi pariatur.</p>
+<button>click here</button>
+    </div>
+    </>
+  )
+}
+export default App
---
+++ styling/src/Cards.jsx
@@ -0,0 +1,30 @@
+import './App.css'
+function Cards(){
+    return(
+        <>
+        <div className="xyz">
+            <div className="a">
+                <h1>hello</h1>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet harum consequuntur magni labore quaerat molestiae incidunt excepturi ducimus nam ipsum possimus accusantium sapiente, fugiat aut quibusdam aperiam itaque dolor sint?</p>
+                 <button>click here</button>
+        </div>
+        <div className="b">
+<h1> everyone</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit illum vero corporis asperiores ut! Quo voluptate, eum laboriosam similique distinctio consectetur assumenda nostrum eos natus exercitationem esse illo! Ipsum, temporibus.
+</p>
+<button>click here</button>
+        </div>
+        <div className="c">
+            <h1>bhopal</h1>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad deleniti sapiente perferendis omnis veritatis quae quasi qui. Aspernatur accusantium suscipit, dignissimos accusamus molestiae vel dolor reprehenderit aliquid, obcaecati atque harum!</p>
+            <button>click here</button>
+
+        </div>
+
+
+        </div>
+        
+        </>
+    )
+}
+export default Cards;
