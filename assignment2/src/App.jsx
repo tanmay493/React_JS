e35dcd8f4c44b9ab6d43b8217c3459e33ee7d98b
tanmay493 <tanmaylanjewar24874@gmail.com>
2026-03-22 17:03:33 +0530

    assignment2

---
+++ assignment2/src/App.jsx
@@ -0,0 +1,25 @@
+function App(){
+    return(
+        <>
+        <div className="bg-black">
+
+        
+    
+         <div className="flex p-40 gap-32  ">
+         <div>
+            <h1 className="text-xs text-white font-semibold ">Currently available for freelance<br/>worldwide</h1>
+            <hr className='text-white'/>
+            <br />
+             <h1 className="text-6xl  text-white font-extrabold font-serif"> Creative Visual <br /> Designer</h1>
+        </div>        
+        <div>
+            <img src="moon (1).jpeg" alt="" className="h-32" />
+        </div>
+        </div>
+        </div>
+        </>
+
+
+    )
+}
+export default App
